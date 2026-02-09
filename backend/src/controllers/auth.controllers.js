305961b9c28e643fe9ee8d0363a0355c7a934056
theofficialvishal <theofficialvishalraj@gmail.com>
2026-02-09 23:57:48 +0530

    Added some best practices

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -36,8 +36,11 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      // generateToken(newUser._id, res);
+      // await newUser.save();
+
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,

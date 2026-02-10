8026ec030101e924cd5c8fa2ee82ff8d8dbe6cb0
theofficialvishal <theofficialvishalraj@gmail.com>
2026-02-10 22:51:33 +0530

    added profile logic

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,7 +15,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //MS 7 days
     httpOnly: true, //prevent XSS attacks: cross-site scripting3
     sameSite: "strict", //CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    secure: NODE_ENV === "development" ? false : true,
   });
   return token;
 };

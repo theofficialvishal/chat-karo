5aa750f16479022b1c8aed8598f3538b6e940fe6
theofficialvishal <theofficialvishalraj@gmail.com>
2026-02-10 23:54:53 +0530

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,19 +6,23 @@ import {
   updateProfile,
 } from "../controllers/auth.controllers.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
+
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
-
 router.put("/update-profile", protectRoute, updateProfile);
-
 router.get("/check", protectRoute, (req, res) =>
   res.status(200).json(req.user),
-);
+); //agar user refresh karta hai toh wo same page pe rahega
 
+/* Testing route
+router.get("/test", arcjetProtection, (req, res) => {
+  res.status(200).json({ message: "Test route" });
+});
+*/
 export default router;

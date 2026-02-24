27e7a304a0d962d0ebf3af7555582e19e5a44073
theofficialvishal <134837701+theofficialvishal@users.noreply.github.com>
2026-02-24 13:20:43 +0530

    chat page layout setup

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,47 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set, get) => ({
+  allContacts: [],
+  chats: [],
+  messages: [],
+  activeTab: null,
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessageLoading: false,
+  isSoundEnabled: localStorage.getItem("isSoundEnabled") === true,
+
+  toggleSound: () => {
+    localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
+    set({ isSoundEnabled: !get().isSoundEnabled });
+  },
+
+  setActiveTab: (tab) => {
+    set({ activeTab: tab });
+  },
+  setSelectiveUser: (selectedUser) => set({ selectedUser }),
+
+  getAllContacts: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/contacts");
+      set({ allContacts: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+  getMyChatPartners: async () => {
+    et({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/chats");
+      set({ allContacts: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+}));

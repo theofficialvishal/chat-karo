4fc7d72ab2042c822f392d80fb78c401cda36e5a
theofficialvishal <134837701+theofficialvishal@users.noreply.github.com>
2026-03-05 17:26:05 +0530

    online messages completed

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import userModel from "../models/user.model.js";
 
@@ -78,6 +79,12 @@ export const sendMessage = async (req, res) => {
     });
     await newMessage.save();
     //todo: send message in real-time if user is online - socket.io
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller : ", error.message);
